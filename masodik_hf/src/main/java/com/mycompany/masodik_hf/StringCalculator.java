01998f7dea4429e45220fec16a67ca18faaf6a66
BalazsMartonZoltan <sportcsoki@gmail.com>
2015-02-24 00:40:19 +0100

    A metódus negatív számnál kivételt dob.

--- masodik_hf/src/main/java/com/mycompany/masodik_hf/StringCalculator.java
+++ masodik_hf/src/main/java/com/mycompany/masodik_hf/StringCalculator.java
@@ -6,6 +6,7 @@
 package com.mycompany.masodik_hf;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -17,11 +18,22 @@ public class StringCalculator {
         int osszeg=0;
         if (!numbers.isEmpty()) {
             String[] numbersStringList = numbers.split(",|\\;|\\\n|\\|");
+            List<Integer> negativeNumbers = new ArrayList<>();
             
             for (String stringnumber : numbersStringList) {
                 int intnumber=Integer.parseInt(stringnumber);
-                
+                if(intnumber<0){
+                  negativeNumbers.add(intnumber);
+                }
                 osszeg+=intnumber;
+            }
+            if(negativeNumbers.size()>0){
+                
+                System.out.print("Negatives not allowed:");
+                for (int i = 0; i < negativeNumbers.size()-1; i++) {
+                    System.out.print(negativeNumbers.get(i)+", ");
+                }
+                return negativeNumbers.get(negativeNumbers.size()-1);
             }
             return osszeg;
         }
